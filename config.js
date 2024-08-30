cbb7bf627d1a2ee423c6fb19305988966ba45bd5
Alekhya <alekhya2120@gmail.com>
2024-08-30 14:53:23 +0000

    Weather app

---
+++ config.js
@@ -0,0 +1,7 @@
+const constants = {
+  openWeatherMap: {
+    BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+    SECRET_KEY: "1a71d16a90d41feffc4ebab1dd1971ff",
+  },
+};
+module.exports = constants;
